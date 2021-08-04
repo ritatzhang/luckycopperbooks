dc0ba470e23991bebcab85fa61f74eb55f2a456f
ritatzhang <ritatzhang@gmail.com>
2021-08-03 20:09:33 -0400

    first books added

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-const composePlugins = require('next-compose-plugins');
-const mdxEnhanced = require('next-mdx-enhanced');
+const composePlugins = require("next-compose-plugins");
+const mdxEnhanced = require("next-mdx-enhanced");
 
-module.exports = composePlugins([
-  mdxEnhanced({
-    layoutPath: './templates'
-  })
-], {
-  images: {
-    domains: ['www.nasa.gov'],
+module.exports = composePlugins(
+  [
+    mdxEnhanced({
+      layoutPath: "./templates",
+    }),
+  ],
+  {
+    images: {
+      domains: ["www.nasa.gov", "i.gr-assets.com"],
+    },
   }
-});
+);
--- templates/post.js
+++ templates/post.js
@@ -1,31 +1,26 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Post({ children, frontMatter }) {
-  const { title } = frontMatter;
+  const { title, author } = frontMatter;
   return (
     <div className={styles.container}>
       <Head>
-        <title>{ title }</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          { title }
-        </h1>
-        <div>
-          { children }
-        </div>
+        <h1 className={styles.title}>{title}</h1>
+        <h2> {author} </h2>
+        <div className={styles.review}> {children} </div>
         <p>
           <Link href="/">
-            <a>
-              Back to home
-            </a>
+            <a className={styles.card}>Back to home</a>
           </Link>
         </p>
       </main>
     </div>
-  )
+  );
 }
